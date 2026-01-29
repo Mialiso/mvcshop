654a621994985f852af9a068a7036e490096ad28
Mialisoa <mialisoarazaf@gmail.com>
2026-01-29 10:58:46 +0100

    init

---
+++ src/main/java/fr/iut/mvcshop/MvcShopApplication.java
@@ -0,0 +1,13 @@
+package fr.iut.mvcshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcShopApplication.class, args);
+	}
+
+}
